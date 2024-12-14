bfb55b827266fa4de8b86319dd637b7ef3dd47da
Alara <Alara>
2024-12-14 12:38:25 -0800

    Co-authored-by: Ayberk <rootroxox@users.noreply.github.com>

---
+++ C++/Courses/Course-1/Code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std; // removes the need to write std::cout everytime in my code
+
+int main() {
+    // cout << "Hello, World!" << endl;
+    
+    // string name; // string is not a built-in data type in C++, it is a part of the standard library
+    // // string is a class, it is not a primitive data type like int, char, double, etc.
+
+    // cout << "Enter your name: "; // no need to define a char array in C++, you can directly use 'string' variable type
+    // cin >> name; //cin is input operator (no need for fgets)
+
+    // cout << "Hello, " << name << "!" << endl;
+
+    // Ask user to enter his name, age and salary and print them
+    /*
+        Expected Output:
+        Enter your name: John
+        Enter your age: 25
+        Enter your salary: 25000
+
+        Name: John
+        Age: 25
+        Salary: 25000    
+    */
+
+    // string name;
+    // cout << "Enter your name: ";
+    // cin >> name;
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // int salary;
+    // cout << "Enter your salary: ";
+    // cin >> salary;
+
+    // cout << "Name: " << name << endl;
+    // cout << "Age: " << age << endl;
+    // cout << "Salary: " << salary << endl;
+
+    return 0;
+}
